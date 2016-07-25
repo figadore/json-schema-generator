804b613d5469b32ce22004b433ed46c7bdc4b3f1
Reese Wilson <reese@shinymayhem.com>
2016-07-25 11:44:55 -0700

    add cli bin 'schemagen'

---
+++ cli/generate.js
@@ -0,0 +1,39 @@
+/* eslint-env node */
+
+// Require external modules
+var program = require('commander');
+
+// Require local modules
+var packageJson = require(__dirname + '/../package');
+var generator = require(__dirname + '/../index');
+
+// Parse command line arguments
+program
+  .version(packageJson.version)
+  .usage('schemagen <fileName>')
+  .parse(process.argv);
+
+if (program.args.length === 0) {
+  console.error("No fileName arg specified");
+  program.outputHelp();
+  process.exit(1);
+}
+
+if (program.args.length !== 1) {
+  console.error("Too many args");
+  program.outputHelp();
+  process.exit(1);
+}
+
+generator.generate(program.args[0], function onGenerated(err, schema) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  log(schema);
+  process.exit(0);
+});
+
+function log() {
+  console.dir.call(null, ...arguments, {depth: null});
+}
